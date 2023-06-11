bc9828c655045643a28446c9982634f89b2e6856
Divyansh Bhardwaj <87605149+Dbhardwaj99@users.noreply.github.com>
2023-06-11 17:46:30 +0530

    Perceptron Algorithm done

--- perceptron/not-perceptron.py
+++ perceptron/not-perceptron.py
@@ -1,14 +1,14 @@
 import pandas as pd
 
-weight1 = 10.0
-weight2 = 10.0
-bias = -10.0
+weight1 = 0.0
+weight2 = -20.0
+bias = 10.0
 
 
 # DON'T CHANGE ANYTHING BELOW
 # Inputs and outputs
 test_inputs = [(0, 0), (0, 1), (1, 0), (1, 1)]
-correct_outputs = [False, True, True, True]
+correct_outputs = [True, False, True, False]
 outputs = []
 
 # Generate and check output
@@ -17,7 +17,6 @@ for test_input, correct_output in zip(test_inputs, correct_outputs):
     output = int(linear_combination >= 0)
     is_correct_string = 'Yes' if output == correct_output else 'No'
     outputs.append([test_input[0], test_input[1], linear_combination, output, is_correct_string])
-
 
 # Print output
 num_wrong = len([output[4] for output in outputs if output[4] == 'No'])
